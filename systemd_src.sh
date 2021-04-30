f259515480e8d05e59919c123174e6db27febba2
Hydra <gool811209@gmail.com>
2021-04-30 11:28:02 +0800

    　which is used with docker-dokuwiki.service

---
+++ systemd_src.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+search_dir='/home/testforta01/SRC_D'
+#search_dir=$(eval echo ~$USER)'/SRC_D'
+#echo $search_dirA
+search_dir=$(pwd)'/SRC_D'
+for entry in "$search_dir"/*;
+do
+	#basename= "$entry"
+	A=${entry##*/}
+	#echo $A
+	if [[ $A == "tmp"* ]];
+	then
+		sudo rm $entry
+		echo $A	
+	fi
+done
+
+#delect tmp*.py pyc file
+find . -name "*tmp*.py" -exec rm -f {} \;
+find . -name "*.pyc" -exec rm -f {} \;
+echo '####################################################'
+echo 'Stopping running containers (if available)...'
+echo '####################################################'
+docker container stop $(docker ps -aq)
+#docker container rm $(docker ps -aq)
+#docker run  --rm --mount type=bind,source="$search_dir",target=/tmp --name SRC_Server diloper/tensorflow_evn:latest python demo.py
+sys_arg=$(hostname)
+#docker run  -d --rm --mount type=bind,source="$search_dir",target=/tmp --name SRC_Server diloper/tensorflow_evn:latest python demo.py $sys_arg
+#sudo shutdown -h now
+#while [[ -d /proc/$(pgrep python) ]]; do sleep 1; done; poweroff
+counter=0
+#while [[ -d /proc/$(pgrep python) ]]; 
+
+while true; 
+do sleep 20;
+A=$(sudo docker ps | grep 'python demo.py')
+#echo $A
+if [ -z "$A" ];
+then
+	echo "empty";	
+	counter=$((counter+1))
+	echo "$counter"
+	if [ $counter -gt 3 ];
+	then
+		echo "$counter"
+		sudo shutdown -h now	
+	fi
+
+	
+else
+	echo $A;	
+fi	
+done; 
