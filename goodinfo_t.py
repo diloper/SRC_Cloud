7ecb7567aaad726e6c817ebe40ae3c161a3731bb
Hank <gool811209@gmail.com>
2022-09-06 08:01:01 +0000

    upload new goodinfo_t.py

--- goodinfo_t.py
+++ goodinfo_t.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[26]:
+# In[14]:
 
 
 # %load goodinfo_t.py
-
-from datetime import date, timedelta, datetime
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import json
+from datetime import date, timedelta, datetime
 import sqlite3
-import time
-import numpy as np
-def create_cookies():
-    fixed_value=';SCREEN_SIZE=WIDTH=1093&HEIGHT=615'
-    today=date.today()
-    Q=today.strftime("%Y%m%d%H%M%S")  # 格式化日期
-    # print(Q)
-    A=requests.get("https://api.ipify.org/?format=json")
-    # A.text
-    # my_ip = load(A.text)['ip']
-    # jsDumps = json.dumps(A.text)  
-    jsLoads = json.loads(A.text)
-    public_ip=jsLoads['ip']
-    # public_ip
-    cookies=Q+'_'+public_ip+fixed_value
-    cookies
-    return cookies
 def save2sqlite(dir,stockid,data):
     
     sqlite_name=dir+str(stockid)+".sqlite"
@@ -52,8 +33,7 @@ def save2sqlite(dir,stockid,data):
         data.to_sql("OHLC", db_handler, if_exists='replace',index=False)
     db_handler.close()
 def get_OHLC_goodinfo_date(stockid,start,end):
-#    df=get_OHLC_goodinfo(stockid)
-    df=retry_download(stockid,counter=3)
+    df=get_OHLC_goodinfo(stockid)
     cond0=df['Date']<= end
     cond1=df['Date']>= start
     # df.loc[start:end]
@@ -61,24 +41,14 @@ def get_OHLC_goodinfo_date(stockid,start,end):
     return A
 #  Add referer in http header to get OHLC data
 def get_OHLC_goodinfo(stockid):
-#     user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36
-# accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
-#     cookie: CLIENT%5FID=20200322103548941%5F221%2E120%2E80%2E192; _ga=GA1.2.1822776169.1584844609; __gads=ID=309d3c3497b843f7:T=1584844609:S=ALNI_MYg4dVQ6Ha-FLVYPN0-Df0MpgpOKw; LOGIN=EMAIL=gool811209%40gmail%2Ecom&USER%5FNM=Herka+Lip&ACCOUNT%5FID=108894270159705601497&ACCOUNT%5FVENDOR=Google&NO%5FEXPIRE=T; SCREEN_SIZE=WIDTH=1280&HEIGHT=720; _gid=GA1.2.1377085740.1594899488; GOOD%5FINFO%5FSTOCK%5FBROWSE%5FLIST=20%7C8916%7C4564%7C2412%7C1434%7C8069%7C3003%7C3303%7C4760%7C2454%7C2633%7C3056%7C6177%7C9188%7C4303%7C2596%7C2317%7C4938%7C8086%7C9945%7C1303
-#     cookie: CLIENT_ID=20200718101954015_36.228.106.174;SCREEN_SIZE=WIDTH=1093&HEIGHT=615
-    
-
-    my_headers = {'cookie': create_cookies()
-        ,'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
-        ,'referer': 'https://goodinfo.tw/StockInfo/ShowK_Chart.asp?STOCK_ID='+str(stockid)}
+    my_headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',        'referer': 'https://goodinfo.tw/StockInfo/ShowK_Chart.asp?STOCK_ID='+str(stockid)}
     url='https://goodinfo.tw/StockInfo/ShowK_Chart.asp?STOCK_ID='+str(stockid) +'&CHT_CAT2=DATE'
     s = requests.Session()
-    Resp=s.get(url,headers =my_headers)
-    
+    Resp=s.post(url,headers =my_headers)
     Resp.encoding =  'utf-8' #for chinese encode
-#     print(Resp.text)
     soup = BeautifulSoup(Resp.text, features="html.parser")
     _script_list = soup.findAll('script')
-    # print(soup.find('script')['src'])
+#     print(soup.find('script')['src'])
     Resp=""
     R=""
     for item in _script_list:
@@ -91,7 +61,10 @@ def get_OHLC_goodinfo(stockid):
                 break
     # substring Javascipt with start_index end_index ,then store at data_list
     result=R.text
-    start_index=result.find('Records:{RPT_TIME')
+#     print(result)
+    key_word='RPT_TIME:['
+    start_index=result.find(key_word)
+#     start_index=result.find('Records:{RPT_TIME')
     end_index=result.find('};',start_index)
     result=result[start_index: end_index+1]
     search_key_word=["RPT_TIME","開盤價","最高價","最低價","收盤價","漲跌價"
@@ -102,102 +75,42 @@ def get_OHLC_goodinfo(stockid):
             continue
         elif "成交額" in search_key_word[i]:
             break
-        start_index =result.find(search_key_word[i])+len(search_key_word[i]+":")+1
-        end_index =result.find(search_key_word[i+1])-2
+        start_index =result.find(search_key_word[i]+":")+len(search_key_word[i]+":")+1
+        end_index =result.index("],", start_index);
         data_list[search_key_word[i]]=result[start_index:end_index].replace('"','').replace('/','-').split(',')
     
     
-    
+#     print(data_list)
     # chage column name
     dataframe_column_name=["Date","Open","High","Low","Close","Volume"]
     Ksearch_key_word=["RPT_TIME","開盤價","最高價","最低價","收盤價","成交量"]
     df = pd.DataFrame()
     for i in range(0,len(dataframe_column_name),1):
         df[dataframe_column_name[i]] = data_list[Ksearch_key_word[i]]
-
-
-#  check df rows
-    rows=df.shape[0]
-    if rows < 1:
-        return False
+#     print(df)
     # chage column data type     
-    try:
-        for i in range(1,len(dataframe_column_name),1):
-            
-                if 'Volume' in  dataframe_column_name[i]:
-                    df = df.astype({dataframe_column_name[i]: int})
-                else:
-                    df = df.astype({dataframe_column_name[i]: float})
-#   check if nan or inf
-                    
-                    
-        P=df.drop(['Date'], axis=1)
-        count = np.isinf(P).values.sum()
-        count_nan_in_df = P.isnull().sum().sum()
-        if  count > 0 or count_nan_in_df > 0:
-            col_name = P.columns.to_series()[np.isinf(P).any()]
-            print(count)
-            print(col_name) 
-            return False
-    except Exception as E:
-        print('Error :', E)
-        return False
-#            print(df)
-            
-#            print(df.shape[0])
-#            server error 500
-
+    for i in range(1,len(dataframe_column_name),1):
+        if 'Volume' in  dataframe_column_name[i]:
+#             df = df.astype({'Discount':'int'})
+            df = df.astype({dataframe_column_name[i]: float})
+            df =df.round({dataframe_column_name[i]:0})
+            df = df.astype({dataframe_column_name[i]: int})
+        else:
+            df = df.astype({dataframe_column_name[i]: float})
 
 
     # 成交量*1000 換算單位將 張轉為股
     df['Volume']=df['Volume']*1000
 
     return df
-
-def retry_download(stockid,counter):
-#     counter=3
-    while counter>0:
-#    if counter>0:
-        df=get_OHLC_goodinfo(stockid)
-        if df is not False:
-            break
-        else:
-            time.sleep(1)#sleep 1 s then retry 
-        counter=counter-1
-    if counter == 0:
-        raise Exception('downlaod fail')
-    return df
-# get last_num
-def get_data_last_row(stockid,last_row):
-    A=retry_download(stockid=stockid,counter=3)    
-    rows=A.shape[0]
-    if last_row>rows:
-        result=A
-    else :
-        result=A.tail(last_row)
-    return result
 def main():
-    STOCK_ID=1303
-    _start=date(2020,4,1)
+    STOCK_ID=2412
+    _start=date(2022,5,24)
     start = _start.isoformat()
     current_date = (_start+timedelta(days=10)).isoformat()
-    count=1
-    while True:
-        A=get_OHLC_goodinfo(STOCK_ID)
-        time.sleep(1)#sleep 1 s then retry 
-        print(count)
-        count=count+1
-        if A is False:
-            break
+
     A=get_OHLC_goodinfo_date(STOCK_ID,start,start)
     print(A.head())
-    get_data_last_row(stockid=STOCK_ID,last_row=200)
 if __name__ == '__main__':
     main()
-
-
-# In[ ]:
-
-
-
 
