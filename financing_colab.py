07d5a6038d02f4dcc91f870f3d294231799f49a4
Hank <gool811209@gmail.com>
2022-10-26 11:15:16 +0000

    merge upadte finiance sqlite

--- financing_colab.py
+++ financing_colab.py
@@ -1,4 +1,10 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# %load financing_colab.py
 
 #!/usr/bin/env python
 # coding: utf-8
@@ -17,18 +23,263 @@
 #(https://www.twse.com.tw/exchangeReport/MI_MARGN?response=json&date=20220104&selectType=ALL)資料最早可追朔2016年
 #將 2016年至今的資料 下載存至DB
 
-import requests
 
+import gzip
+import io
+import numpy as np
+import os
+import requests
 import pandas as pd
 from datetime import date, timedelta, datetime
 import sqlite3
 from time import sleep
 import json
-import os
 import goodinfo_t
-
 import upload_file
-import gzip
+import progressbar
+dir="./financing/"
+
+
+dir='./small_test/'
+stockid='2330'
+
+
+def readOHCLsql(dir,stockid,sql=None):
+    price_db_name=dir+str(stockid)+".sqlite"
+    # if os.path.isfile(price_db_name) is not True:
+    #     return
+    db_handler = sqlite3.connect(price_db_name)
+    # tat='SELECT OHLC.Date,OHLC.Close,SRC.M20 from OHLC WHERE Date <="'+ str(date) +'"order by Date desc  LIMIT "'+str(boundary)+'"'
+    tat='SELECT OHLC.Date,OHLC.Close from OHLC'
+    # df = pd.read_sql_query('SELECT OHLC.Date,OHLC.Close, SRC.M20 from SRC INNER JOIN OHLC ON SRC.Date = OHLC.Date order by SRC.Date ASC',db_handler)
+    # df['20 Day MA']=df['Close'].rolling(window=20,min_periods=20).mean()
+    dfA = pd.read_sql_query(tat,db_handler)
+    db_handler.close()
+    return dfA
+
+
+def savetosql(dir,stockid,dfH):
+#  K_columns=['stockid','buy','sell','yesterday_balance','today_balance','Date']
+    price_db_name=dir+str(stockid)+".sqlite"
+    # if os.path.isfile(price_db_name) is not True:
+    #     return
+    db_handler = sqlite3.connect(price_db_name)
+    cursor = db_handler.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS "financing"      (`buy` REAL  NOT NULL,`sell` REAL NOT NULL,    `yesterday_balance`INTEGER NOT NULL,`today_balance`INTEGER NOT NULL,    `fin_cost`REAL NOT NULL,`fin_maintenance_rate`REAL NOT NULL,    `Date`TEXT  PRIMARY KEY NOT NULL)')
+#     cursor.execute('SELECT name FROM  sqlite_master    WHERE type ="table" And name = "financing"')
+#     rows = cursor.fetchall()
+#     if len(rows)>0:
+#     dfH.to_sql('financing', db_handler, if_exists='append', index = False)
+#     else:
+    try:
+        
+        sql=list(dfH.columns.values)
+        columan=""
+        values=""
+        for i in range(len(sql)):
+            columan=columan+',"'+sql[i]+'"'
+            values=values+",?"
+#         print(columan[1:len(columan)])
+#         print(values[1:len(values)])
+#         print(dfH.values.tolist())
+#         """Date	buy	fin_cost	fin_maintenance_rate	sell	today_balance	yesterday_balanceinsert into XXXXXXXXXXXX("XXXXXXXXXX", "XXXXXXXXX", "XXXXXXXXXXX") values(?,?,?)"""
+        cursor.executemany('insert into financing ('+columan[1:len(columan)]+') values ('+values[1:len(values)]+')', dfH.values.tolist())
+#         cursor.executemany('insert into financing   values (?,?,?,?,?,?,?)', dfH.values.tolist())
+        db_handler.commit()
+    except Exception as E:
+        print('Error : ', E)
+        db_handler.close()
+#         dfH.to_sql('financing', db_handler, if_exists='replace', index = False)
+    db_handler.close()
+
+
+def readfromsql(dir,stockid,dateformate=False,sql=None):
+
+    price_db_name=dir+str(stockid)+".sqlite"
+    # if os.path.isfile(price_db_name) is not True:
+    #     return
+    db_handler = sqlite3.connect(price_db_name)
+    cursor = db_handler.cursor()
+    cursor.execute('SELECT name FROM  sqlite_master WHERE type ="table" And name = "financing"')
+    rows = cursor.fetchall()
+    if len(rows)>0:
+        if sql is not None:
+            cursor.execute(sql)
+            dfB = pd.DataFrame(cursor.fetchall(), columns=['buy','sell','yesterday_balance',
+                                                       'today_balance','fin_cost',
+                                                       'fin_maintenance_rate','Date','Close'])   
+        else:
+            cursor.execute('SELECT * FROM financing' )
+        # for row in cursor.fetchall():    
+        #     print (row) K_columns=[','buy','sell','yesterday_balance','today_balance','Date']
+            dfB = pd.DataFrame(cursor.fetchall(), columns=['buy','sell','yesterday_balance',
+                                                       'today_balance','fin_cost',
+                                                       'fin_maintenance_rate','Date'])   
+        if(dateformate):
+            dfB['Date'] = pd.to_datetime(dfB.Date)# change the datetime format
+        db_handler.close()
+        return dfB
+    else:
+        db_handler.close()
+        return None
+
+
+#  download from google drive
+def D_from_drive(date):
+    D_Handel=upload_file.Google_Driver_API()
+    dt_obj = datetime.strptime(str(date),'%Y%m%d')
+    yearfolder=str(dt_obj.year)
+    folder_id=D_Handel.search_folder(name='financing')
+    downloadfolder_id=D_Handel.search_folder(yearfolder,folder_id)
+    if downloadfolder_id is not None:
+        name=str(date)+".gz"
+        file_id=D_Handel.search_file(name,folder_id=downloadfolder_id)
+        if file_id is not None:
+            _IO=D_Handel.downloadFile(file_id=file_id,local_filepath=None,IO=True)
+            f=gzip.decompress(_IO)
+            s = f.decode('UTF-8')
+            return s
+    return None
+
+def get_financing_v2(date,stockid):
+#     local file
+    try:
+        dt_obj = datetime.strptime(str(date),'%Y%m%d')
+        yearfolder=str(dt_obj.year)
+        file_path='./financing/'+date+'.gz'
+
+        if os.path.isfile(file_path):
+
+            with gzip.open(file_path, 'rb') as f:
+                file_content = f.read()
+                A = file_content.decode('UTF-8')
+        else: 
+            A=D_from_drive(date)
+
+        result=A
+
+        y = json.loads(result)
+    except:
+        print(result)
+        return None
+    df = pd.DataFrame (y['data'], columns = ['股票代號', '股票名稱', '買進', '賣出', '現金償還', '前日餘額', '今日餘額', '限額', '買進V', '賣出V', '現券償還', '前日餘額V', '今日餘額V', '限額', '資券互抵', '註記'])
+
+    del_target=[ '股票名稱', '現金償還','買進V', '賣出V', '現券償還', '前日餘額V', '今日餘額V', '限額', '資券互抵', '註記']
+
+    for i in del_target:
+        del df[i]
+    G=df.query('股票代號 == "'+stockid+'"')
+    if G.shape[0] == 0:
+        return None
+    return G
+_BN=120
+# 列出 之前N筆資料
+def find_BN_OHCL(stockid,date,n):
+    A=goodinfo_t.get_OHLC_goodinfo(stockid)
+
+    df=A
+    # df.set_index('Date')
+
+    cond0=df['Date'] == str(date)
+    # cond1=df['Date']>= start
+    # df.loc[start:end]
+    x=df.loc[cond0 ].index[0]
+    if (x-n)<0:
+        return None
+    
+    return df.iloc[x-n:x]
+# flag is True return back half value
+def fin_maintenance_rate(df,flag):
+    df.replace({',':''}, regex=True,inplace=True)
+    df = df.astype({'yesterday_balance':'float'})
+    df = df.astype({'buy':'float'})
+    # df = df.astype({'financing':'float'})
+    # 設定初始值
+    
+    # df['fin_cost'][0]=df["Close"][0] 會觸發SettingWithCopyWarning
+    if flag:
+        df['fin_cost']= np.nan
+        df.loc[0:1,['fin_cost']]=df["Close"][0]
+
+        
+    df.reset_index(inplace=True)
+    df.drop(['index'], axis=1,inplace=True)
+    for i in range(len(df)-1):
+        fin_cost=(df["buy"][i+1]*df["Close"][i+1]+df["fin_cost"][i]*df["yesterday_balance"][i+1])/(df["buy"][i+1]+df["yesterday_balance"][i+1])
+        fin_maintenance_rate=df["Close"][i+1]/(np.round(fin_cost,4)*0.6)*100
+        df.loc[i+1:i+2,"fin_cost"]=np.round(fin_cost,4)
+        df.loc[i+1:i+2,"fin_maintenance_rate"]=np.round(fin_maintenance_rate,4)
+    if flag:
+        return df.loc[len(df)/2:len(df)-1]
+    return df.loc[1:len(df)-1]
+                                  
+
+def update_financingsql(dir,stockid):
+# downlaod / update new data
+
+    dfA=readOHCLsql(dir,stockid)
+    dfB=readfromsql(dir,stockid)
+    find_BN=False
+    Adf=None
+    _columns=['股票代號','買進','賣出','前日餘額','今日餘額','Date']
+    K_columns=['stockid','buy','sell','yesterday_balance','today_balance','Date']
+    if dfB is not None and dfA.shape[0]>0: #     if financing table is exist
+#         dfA.replace({'-':''}, regex=True,inplace=True)
+        df_1notin2 = dfA[~(dfA['Date'].isin(dfB['Date']) )].reset_index(drop=True)
+#         print(df_1notin2)
+        if df_1notin2.shape[0] < 1:
+            return 0
+        _sql='select financing.* , OHLC.Close from  financing join OHLC  on OHLC.date=financing.date  WHERE financing.date < "'+str(df_1notin2['Date'][0])+'" order by  financing.date desc limit 1'
+
+#         print(str(df_1notin2['Date'][0]))
+        G=readfromsql(dir,stockid,sql=_sql)
+        Adf = pd.DataFrame(G)
+
+
+    else:
+        df_1notin2=dfA
+#         print(df_1notin2['Date'][0])
+        tmp=find_BN_OHCL(stockid,df_1notin2['Date'][0],_BN)
+        df_1notin2=pd.concat([tmp.loc[:,['Date','Close']],df_1notin2]).reset_index(drop=True)
+        find_BN=True
+    dfG = pd.DataFrame(columns=_columns)
+   
+
+    for index in range(len(df_1notin2)):
+        date=df_1notin2['Date'][index]
+#         print(date)
+        row=get_financing_v2(date=date.replace("-", ""),stockid=stockid)# convert Y-M-D to YMD
+        if row is not None:
+            row.reset_index(drop=True,inplace=True)
+            row['Date'] = date
+            row['Close']=df_1notin2['Close'][index]
+    #         print(row)
+            dfG=pd.concat([dfG, row])
+#         else:
+#             print('cant find'+str(date))
+            
+            
+
+    if  dfG.shape[0]>0:
+        dfH=dfG.drop(columns=['股票代號'])
+        dfH.rename(columns = {'買進' :'buy','賣出':'sell', '前日餘額':'yesterday_balance','今日餘額':'today_balance'}, inplace = True)
+        
+        if Adf is not None:
+            dfH=pd.concat([Adf,dfH]).reset_index(drop=True)
+
+        K=fin_maintenance_rate(dfH,find_BN)
+        
+        K.drop(['Close'], axis=1,inplace=True)
+
+        savetosql(dir,stockid,K)
+    else:
+        print('cant find financing'+str(stockid))
+
+
+
+
+
+
 
 
 # can't use => "import import goodinfo_t as A"
@@ -60,153 +311,40 @@ def get_financing_v1(date='20220104'):
         return None   
     return Resp
 
-def get_financing_v2(date='20220104',stockid='0050'):
 
-   
-    Resp=get_financing_v1(date)
-    y = json.loads(Resp.text)
-    df = pd.DataFrame (y['data'], columns = 
-              ['股票代號', '股票名稱', '買進', '賣出','現金償還', '前日餘額', '今日餘額', '限額',
-                            '買進V', '賣出V', '現券償還', '前日餘額V', '今日餘額V', '限額V', '資券互抵', '註記'])
-
-    del_target=[ '股票名稱', '現金償還','買進V', '賣出V', '現券償還', '前日餘額V', '今日餘額V', '限額', '限額V', '資券互抵', '註記']
-
-    for i in del_target:
-        del df[i]
-    
-    return df.query('股票代號 == "'+stockid+'"')
-
-
-
-# get_financing_v2(date='20220923',stockid=stockid)
-
-
-def readOHCLsql(stockid):
-    dir='./small_test/'
-
-    price_db_name=dir+str(stockid)+".sqlite"
-    if os.path.isfile(price_db_name) is not True:
-        return
-    price_db_handler = sqlite3.connect(price_db_name)
-    # tat='SELECT OHLC.Date,OHLC.Close,SRC.M20 from OHLC WHERE Date <="'+ str(date) +'"order by Date desc  LIMIT "'+str(boundary)+'"'
-    tat='SELECT OHLC.Date,OHLC.Close from OHLC'
-    # df = pd.read_sql_query('SELECT OHLC.Date,OHLC.Close, SRC.M20 from SRC INNER JOIN OHLC ON SRC.Date = OHLC.Date order by SRC.Date ASC',price_db_handler)
-    # df['20 Day MA']=df['Close'].rolling(window=20,min_periods=20).mean()
-    dfA = pd.read_sql_query(tat,price_db_handler)
-    price_db_handler.close()
-    return dfA
-
-
-def savetosql(stockid,dfH):
-    db_handler = sqlite3.connect('financing'+stockid+".sqlite")
-    cursor = db_handler.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS "financing" (`買進` INTEGER  NOT NULL,`賣出` INTEGER NOT NULL,\
-    `前日餘額`TEXT NOT NULL,`今日餘額`TEXT NOT NULL,`Date`TEXT  PRIMARY KEY NOT NULL)')
-#     cursor.execute('SELECT name FROM  sqlite_master    WHERE type ="table" And name = "financing"')
-#     rows = cursor.fetchall()
-#     if len(rows)>0:
-#     dfH.to_sql('financing', db_handler, if_exists='append', index = False)
-#     else:
-    try:
-        cursor.executemany('insert into financing values(?,?,?,?,?)', dfH.values.tolist())
-        db_handler.commit()
-    except Exception as E:
-        print('Error : ', E)
-        db_handler.close()
-#         dfH.to_sql('financing', db_handler, if_exists='replace', index = False)
-    db_handler.close()
-
-def readfromsql(stockid,dateformate=False):
-    db_handler = sqlite3.connect('financing'+stockid+".sqlite")
-    cursor = db_handler.cursor()
-    cursor.execute('SELECT name FROM  sqlite_master WHERE type ="table" And name = "financing"')
-    rows = cursor.fetchall()
-    if len(rows)>0:
-        cursor.execute(' SELECT * FROM financing' )
-        # for row in cursor.fetchall():    
-        #     print (row)
-        dfB = pd.DataFrame(cursor.fetchall(), columns=['買進','賣出','前日餘額','今日餘額','Date'])   
-        if(dateformate):
-            dfB['Date'] = pd.to_datetime(dfB.Date)# change the datetime format
-        db_handler.close()
-        return dfB
-    else:
-        db_handler.close()
-        return 0
-
-
-
-def update_financingsql(stockid):
-# downlaod / update new data
-
-    dfB=readfromsql(stockid)
-    dfA=readOHCLsql(stockid)
-    dfA.replace({'-':''}, regex=True,inplace=True)
-    _columns=['股票代號','買進','賣出','前日餘額','今日餘額','Date']
-    if dfB is not 0:
-        df_1notin2 = dfA[~(dfA['Date'].isin(dfB['Date']) )].reset_index(drop=True)
-    else:
-        df_1notin2=dfA
-
-    dfG = pd.DataFrame(columns=_columns)
-    # dfG
-    for date in df_1notin2['Date']:
-    #     print(date)
-        row=get_financing_v2(date=date,stockid=stockid)
-        if row is not None:
-            row.reset_index(drop=True,inplace=True)
-            row['Date'] = date
-        #     print(row)
-
-            dfG=pd.concat([dfG, row])
-    if  dfG.shape[0]>0:
-        dfH=dfG.drop(columns=['股票代號'])
-        savetosql(stockid,dfH)
-
-# dfB=readfromsql(stockid,dateformate=True)
-# dfA=readOHCLsql(stockid)
-
-
-# dfA
-# dfA.join(dfB, on='Date')
-# R=dfA.set_index('Date').join(dfB.set_index('Date'))
-# pd.merge(dfA,dfB,how='inner',left_on=['Date'],right_on=['Date'])
-
-# update_financingsql(str(2412))
-
-# R.to_csv(stockid+'.csv')
-
-# _start=date.fromisoformat('2021-05-24')
-
-# A['Date']
-
-# get_OHLC_goodinfo_date(STOCK_ID,start,current_date)
-
-dir="./financing/"
 def upload_finace_data(date,D_Handel,folder_id):
-  dt_obj = datetime.strptime(date,'%Y%m%d')
-  yearfolder=str(dt_obj.year)
-  # create folder if not exist
-  uploadfolder_id=D_Handel.search_folder(yearfolder,folder_id)
-  if uploadfolder_id is None:
-    uploadfolder_id=D_Handel.createFolder(yearfolder,folder_id)
-  
-  Res=get_financing_v1(date)
-  bytes_encoded = Res.text.encode(encoding='utf-8')
-  # print(type(bytes_encoded))
-  filename=dir+str(date)+'.gz'
-  with gzip.open(filename, 'wb') as f:
-      f.write(bytes_encoded)
+    dt_obj = datetime.strptime(date,'%Y%m%d')
+    yearfolder=str(dt_obj.year)
+    # create folder if not exist
+    uploadfolder_id=D_Handel.search_folder(yearfolder,folder_id)
+    if uploadfolder_id is None:
+        uploadfolder_id=D_Handel.createFolder(yearfolder,folder_id)
+
+    Res=get_financing_v1(date)
+    bytes_encoded = Res.text.encode(encoding='utf-8')
+#     check if data >0
+    dictx = json.loads(Res.text)
+    train = pd.DataFrame.from_dict(dictx, orient='index')
+    train.reset_index(level=0, inplace=True)
+    cond0=train['index']== 'data'
+    # df.loc[start:end]
+    A=train.loc[cond0 ]
+    if len(A.iat[0, 1]) <1:
+        return None
+    # print(type(bytes_encoded))
+    filename=dir+str(date)+'.gz'
+    with gzip.open(filename, 'wb') as f:
+        f.write(bytes_encoded)
       
-  file_id=D_Handel.search_file(name=filename,folder_id=uploadfolder_id)
-  if file_id is None:
-    D_Handel.uploadFile(filename=str(date)+'.gz',filepath=filename
+    file_id=D_Handel.search_file(name=filename,folder_id=uploadfolder_id)
+    if file_id is None:
+        D_Handel.uploadFile(filename=str(date)+'.gz',filepath=filename
         ,mimetype="application/gzip"
         ,folder_id=uploadfolder_id)
 
   #os.remove(filename)
 
-import progressbar
+
 def main():
     _start = date(2020,1,1)
     today = date.today()
@@ -217,6 +355,7 @@ def main():
     print(start)
     print(current_date)
     A=goodinfo_t.get_OHLC_goodinfo_date(STOCK_ID,start,current_date)
+#     print(A)
     A.replace({'-':''}, regex=True,inplace=True)
     D_Handel=upload_file.Google_Driver_API()
     folder_id=D_Handel.search_folder(name='financing')
@@ -246,15 +385,19 @@ def main():
     # R.drop_duplicates(inplace=True)
 #    print(len(T))
     for item in progressbar.progressbar(T):
-#      print(item)
-      upload_finace_data(item,D_Handel,folder_id)
-
-
+#         print(item)
+        upload_finace_data(item,D_Handel,folder_id)
+dirp='small_test'
+import download_SRC as SRCtool
 if __name__ == '__main__':
     main()
-#with gzip.open(filename, 'rb') as f:
-#    file_content=f.read()
-#
-#str_decoded = file_content.decode(encoding='utf-8')
-#print(str_decoded)
+    
+    A=SRCtool.get_sqlite_local_file(dirp)
+    for item in progressbar.progressbar(A):
+        try:
+#             print(item[0])
+            update_financingsql(dir,stockid=item[0])
+        except Exception as e: 
+            print("except"+item[0])
+            print(e)
 
