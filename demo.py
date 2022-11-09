d9d18a6d42b1c6e86fe42a27b87f43d913e14dc5
Hank <gool811209@gmail.com>
2022-11-09 17:15:36 +0800

    shift get_stockid() to search_tool

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 import sys
-
+import search_tool
 # import bz2
 import pandas as pd
 import io,re,os 
@@ -174,47 +174,6 @@ def create_cookies():
     cookies
     return cookies
 
-def get_stockid(head,tail):
-#     Plan A: requests sometimes fail
-#     my_headers = {'cookie': create_cookies()
-#     ,'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
-    
-#     url = "http://isin.twse.com.tw/isin/C_public.jsp?strMode=2"
-#     res = requests.get(url,headers =my_headers)
-#     plan B :download isin from google drive
-    D_Handel=upload_file.Google_Driver_API()
-    folder_id=D_Handel.search_folder(name='Stock db')
-    file_id=D_Handel.search_file(name='isin.html',folder_id=folder_id)
-    html=D_Handel.downloadFile(file_id=file_id,local_filepath="isin.html",IO=True)
-    del D_Handel
-    text_obj = html.decode('UTF-8')
-
-    df = pd.read_html(text_obj)[0]
-
-    # 設定column名稱
-    df.columns = df.iloc[0]
-    # 刪除第一行
-    # df = df.iloc[2:]
-    # 先移除row，再移除column，超過三個NaN則移除
-    df.dropna(axis='columns',inplace=True)
-    df.dropna(axis='index',inplace=True)
-    # df.head()
-    dfx=df.iloc[:,0:1]    
-    dfx.reset_index(inplace=True)
-    del dfx['index']
-    # 股票
-#     G=dfx.query('有價證券代號及名稱 == "上市認購(售)權證"')
-#    print(dfx.head())
-    G1=dfx.query('有價證券代號及名稱 == "'+head+'"')
-    G2=dfx.query('有價證券代號及名稱 == "'+tail+'"')
-#     index=G.index.values[0]
-#     o=dfx.iloc[0:index,:]
-    index1=G1.index.values[0]
-    index2=G2.index.values[0]
-    k=dfx.iloc[index1+1:index2,0]
-    
-    return k.tolist()
-
 
 def local_file(last_folder):
     # 歷遍資料夾下的的檔案名稱，符合後紀錄於local_files
@@ -251,7 +210,7 @@ def main():
     # local_files=local_file(last_folder=date_y)
     print(date_y)
     local_files=local_file(date_y)
-    stockid_list=get_stockid('股票','上市認購(售)權證')
+    stockid_list=search_tool.get_stockid('股票','上市認購(售)權證')
 
     folder_id=None
     date_y=''
@@ -436,7 +395,7 @@ def delet_folder(remain_num,check_Empty=True,dir_target='./',file=False):
                 os.rmdir(path)
         else:
             shutil.rmtree(path, ignore_errors=True)
-import search_tool
+
 import Stock_SRC_Small as Stock_Small
 import financing_colab as financing
 oldfilename='SRCresult.csv'
@@ -485,5 +444,5 @@ if __name__ == '__main__':
         logging.error("Catch an exception.", exc_info=True)
         Line_agent(condition=3,message="find logging.error")
 #     notify_service(2)
-#     stockid_list=get_stockid('股票','上市認購(售)權證')
+#     stockid_list=search_tool.get_stockid('股票','上市認購(售)權證')
 
--- search_tool.py
+++ search_tool.py
@@ -1,4 +1,6 @@
-import io,re,os 
+import io,re,os
+import pandas as pd
+import upload_file
 def local_files_v1(dir_target="./",reg=r"\d{4}\d{2}\d{2}"):
 # 歷遍當前目錄下的資料夾名稱 符合:2020-01-01此格式
     directory_list = list()
@@ -39,3 +41,44 @@ def local_folder(dir_target="./",format=None):
                 directory_list.append(os.path.join(root, name))
 
     return directory_list
+
+def get_stockid(head,tail,df_flag=False):
+#     Plan A: requests sometimes fail
+#     my_headers = {'cookie': create_cookies()
+#     ,'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
+    
+#     url = "http://isin.twse.com.tw/isin/C_public.jsp?strMode=2"
+#     res = requests.get(url,headers =my_headers)
+#     plan B :download isin from google drive
+    D_Handel=upload_file.Google_Driver_API()
+    folder_id=D_Handel.search_folder(name='Stock db')
+    file_id=D_Handel.search_file(name='isin.html',folder_id=folder_id)
+    html=D_Handel.downloadFile(file_id=file_id,local_filepath="isin.html",IO=True)
+    del D_Handel
+    text_obj = html.decode('UTF-8')
+
+    df = pd.read_html(text_obj)[0]
+
+    # 設定column名稱
+    df.columns = df.iloc[0]
+    # 刪除第一行
+    # df = df.iloc[2:]
+    # 先移除row，再移除column，超過三個NaN則移除
+    df.dropna(axis='columns',inplace=True)
+    df.dropna(axis='index',inplace=True)
+    # df.head()
+    dfx=df.iloc[:,0:1]    
+    dfx.reset_index(inplace=True)
+    del dfx['index']
+    # 股票
+#     G=dfx.query('有價證券代號及名稱 == "上市認購(售)權證"')
+#    print(dfx.head())
+    G1=dfx.query('有價證券代號及名稱 == "'+head+'"')
+    G2=dfx.query('有價證券代號及名稱 == "'+tail+'"')
+#     index=G.index.values[0]
+#     o=dfx.iloc[0:index,:]
+    index1=G1.index.values[0]
+    index2=G2.index.values[0]
+    k=dfx.iloc[index1+1:index2,0]
+
+    return k.tolist()
